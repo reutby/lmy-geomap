462b0550e89ac7ec6a3ff06b7a94e2e833cd244b
reutby <reutby321@gmail.com>
2021-03-18 11:29:27 +0200

    initial server and db connection

---
+++ server/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+
+// get delete and post request 
+app.get('/', (req,res)=>{
+    res.send('Hello to memories API');
+});
+
+app.post('/', (req,res)=>{
+    res.send('Hello to memories API');
+});
+
+app.delete('/', (req,res)=>{
+    res.send('Hello to memories API');
+});
+
+
+const port = process.env.PORT || 5000;
+
+
+mongoose.connect(process.env.MONGODB_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('useFindAndModify',false);
+
+const db = mongoose.connection;
+
+db.on('error',console.error.bind(console, 'Connection Error:'));
+db.once('open', ()=>{
+    app.listen(port,() => console.log(`server running on port: ${port}`));
+})
+
